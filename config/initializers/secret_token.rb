033ecaa53520a6e43423568e8790fe4d5b852d07
Phil Gengler <phil@pgengler.net>
2012-04-03 14:43:12 -0400

    Newly generated Rails app; no project-specific code or work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logbook::Application.config.secret_token = 'b1cd5799285338d4a76d3c618c241f2169280c06b9d805175aa1c9ab3b654cd9cc84379cd2d9bc78561db50570cb0101c3e389f92fc82bc76525337c9d408304'
