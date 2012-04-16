465f641fc1ab07f530f17434e7c78075e5817bf2
Phil Gengler <phil@pgengler.net>
2012-04-16 10:11:05 -0400

    Modify plane_dropdown helper to prepend a '--' (empty) <option> element

--- app/helpers/planes_helper.rb
+++ app/helpers/planes_helper.rb
@@ -1,7 +1,10 @@
 module PlanesHelper
 
 	def plane_dropdown(form)
-		form.select :plane_id, Plane.all.map { |plane| [ "#{plane.tail_number} (#{plane.model})", plane.id ] }
+		options = Plane.all.map { |plane| [ "#{plane.tail_number} (#{plane.model})", plane.id ] }
+		options.unshift [ '--', 0 ]
+
+		form.select :plane_id, options #[ [ '', nil ], Plane.all.map { |plane| [ "#{plane.tail_number} (#{plane.model})", plane.id ] } ]
 	end
 
 end
--- test/unit/helpers/planes_helper_test.rb
+++ test/unit/helpers/planes_helper_test.rb
@@ -1,4 +1,16 @@
 require 'test_helper'
 
 class PlanesHelperTest < ActionView::TestCase
+
+	test "select box includes empty option at top" do
+		form_for(Entry.new) do |form|
+			select_box = plane_dropdown form
+
+			# This is a complete hack, but Rails doesn't seem to provide a good
+			# way of checking for a subset of the DOM (only assert_dom_equal, which
+			# operates on the entire DOM tree)
+			assert_match /<select.+?>\s*<option value=(['"])0\1>--<\/option>/, select_box
+		end
+	end
+
 end
