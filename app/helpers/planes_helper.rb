b53881ac1621bf9487abda84deb1afea29d8fe65
Phil Gengler <phil@pgengler.net>
2012-04-16 10:18:18 -0400

    Add a 'plane_for_display' helper function for formatting a Plane object as a string for display

--- app/helpers/planes_helper.rb
+++ app/helpers/planes_helper.rb
@@ -2,9 +2,14 @@ module PlanesHelper
 
 	def plane_dropdown(form)
 		options = Plane.all.map { |plane| [ "#{plane.tail_number} (#{plane.model})", plane.id ] }
-		options.unshift [ '--', 0 ]
+		options.unshift [ '--', nil ]
 
 		form.select :plane_id, options #[ [ '', nil ], Plane.all.map { |plane| [ "#{plane.tail_number} (#{plane.model})", plane.id ] } ]
+	end
+
+	# Format Plane object as a string for display
+	def plane_for_display(plane)
+		"#{plane.tail_number} (#{plane.model})"
 	end
 
 end
--- test/unit/helpers/planes_helper_test.rb
+++ test/unit/helpers/planes_helper_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class PlanesHelperTest < ActionView::TestCase
 
+	test "plane_for_display formats Plane object for display" do
+		plane = Plane.new :tail_number => 'N12345', :model => 'C172N'
+
+		assert_equal "N12345 (C172N)", plane_for_display(plane)
+
+	end
+
 	test "select box includes empty option at top" do
 		form_for(Entry.new) do |form|
 			select_box = plane_dropdown form
@@ -9,7 +16,7 @@ class PlanesHelperTest < ActionView::TestCase
 			# This is a complete hack, but Rails doesn't seem to provide a good
 			# way of checking for a subset of the DOM (only assert_dom_equal, which
 			# operates on the entire DOM tree)
-			assert_match /<select.+?>\s*<option value=(['"])0\1>--<\/option>/, select_box
+			assert_match /<select.+?>\s*<option value=(['"])\1>--<\/option>/, select_box
 		end
 	end
 
