17b43d8a3cdb1ff4d0d619b51ba9400e7835bfda
Phil Gengler <phil@pgengler.net>
2014-11-01 15:59:49 -0400

    Clean up PlanesController

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -1,83 +1,43 @@
 class PlanesController < ApplicationController
-  # GET /planes
-  # GET /planes.json
-  def index
-    @planes = Plane.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @planes }
-    end
-  end
+	def index
+		@planes = Plane.all
+	end
 
-  # GET /planes/1
-  # GET /planes/1.json
-  def show
-    @plane = Plane.find(params[:id])
+	def show
+		@plane = Plane.find(params[:id])
+	end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @plane }
-    end
-  end
+	def new
+		@plane = Plane.new
+	end
 
-  # GET /planes/new
-  # GET /planes/new.json
-  def new
-    @plane = Plane.new
+	def edit
+		@plane = Plane.find(params[:id])
+	end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @plane }
-    end
-  end
+	def create
+		@plane = Plane.new(params[:plane])
+		if @plane.save
+			redirect_to @plane, notice: 'Plane was successfully created.'
+		else
+			render action: "new"
+		end
+	end
 
-  # GET /planes/1/edit
-  def edit
-    @plane = Plane.find(params[:id])
-  end
+	def update
+		@plane = Plane.find(params[:id])
 
-  # POST /planes
-  # POST /planes.json
-  def create
-    @plane = Plane.new(params[:plane])
+		if @plane.update_attributes(params[:plane])
+			redirect_to @plane, notice: 'Plane was successfully updated.'
+		else
+			render action: "edit"
+		end
+	end
 
-    respond_to do |format|
-      if @plane.save
-        format.html { redirect_to @plane, notice: 'Plane was successfully created.' }
-        format.json { render json: @plane, status: :created, location: @plane }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @plane.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /planes/1
-  # PUT /planes/1.json
-  def update
-    @plane = Plane.find(params[:id])
-
-    respond_to do |format|
-      if @plane.update_attributes(params[:plane])
-        format.html { redirect_to @plane, notice: 'Plane was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @plane.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /planes/1
-  # DELETE /planes/1.json
-  def destroy
-    @plane = Plane.find(params[:id])
-    @plane.destroy
-
-    respond_to do |format|
-      format.html { redirect_to planes_url }
-      format.json { head :ok }
-    end
-  end
+	def destroy
+		@plane = Plane.find(params[:id])
+		@plane.destroy
+		redirect_to planes_url
+	end
 end
