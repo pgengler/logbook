35558ace5a0fd9aa102d5ac4218734af52d8525c
Phil Gengler <phil@pgengler.net>
2014-12-17 12:17:33 -0500

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,14 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120413201155) do
+ActiveRecord::Schema.define(version: 20120413201155) do
 
-  create_table "entries", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "entries", force: true do |t|
     t.date     "flown"
     t.integer  "plane_id"
     t.string   "from"
@@ -32,17 +35,17 @@ ActiveRecord::Schema.define(:version => 20120413201155) do
     t.decimal  "dual_received"
     t.decimal  "pic"
     t.decimal  "total_flight"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "entries", ["plane_id"], :name => "index_entries_on_plane_id"
+  add_index "entries", ["plane_id"], name: "index_entries_on_plane_id", using: :btree
 
-  create_table "planes", :force => true do |t|
+  create_table "planes", force: true do |t|
     t.string   "tail_number"
     t.string   "model"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
